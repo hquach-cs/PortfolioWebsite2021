7c2faa9a359c5a4b6a1854c3cddd4a7f24836b69
hquach-cs <hquach.cs@gmail.com>
2020-08-06 18:03:38 -0500

    Updates

---
+++ precache-manifest.3ae11685be648b80f4a6d339d7f43b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2054ac56a5c0766f0653fbb5683c7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2849d229fe2ce56b8b2f",
+    "url": "/static/css/main.884d0b00.chunk.css"
+  },
+  {
+    "revision": "22fdc51033e022a6a5c8",
+    "url": "/static/js/2.03dc32a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.03dc32a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2849d229fe2ce56b8b2f",
+    "url": "/static/js/main.106b9fa2.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "be0cd8264447510d449557c1ed8db786",
+    "url": "/static/media/hquachResume.be0cd826.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PortfolioWebsite/precache-manifest.85301d4ae8fd95fe1b9b4abb78c1cf18.js"
+  "/precache-manifest.3ae11685be648b80f4a6d339d7f43b8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PortfolioWebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
